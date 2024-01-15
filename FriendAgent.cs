aedd2a90df9c56ad0e796af54ce9af3b6311c974
Adolfcontinue <109844806+Adolfcontinue@users.noreply.github.com>
2024-01-15 16:17:47 +0900

    [상근] source

---
+++ FriendAgent.cs
@@ -0,0 +1,290 @@
+﻿using System;
+using System.Collections.Generic;
+using SCommon;
+using Global;
+
+namespace PlayServer
+{
+    public class CFriendAgent
+    {
+        private CUser m_Owner;
+
+
+        private Dictionary<long , _FriendData> m_FriendList = new Dictionary<long , _FriendData>();
+        private Dictionary<long , _FriendData> m_ReceivePendingList = new Dictionary<long , _FriendData>();
+        private Dictionary<long , _FriendData> m_SendPendingList = new Dictionary<long , _FriendData>();
+        
+        public void Init(List<_FriendData> friendList, List<_FriendData> recvList, List<_FriendData> sendList)
+        {
+            m_FriendList.Clear();
+            foreach(var data in friendList)
+            {
+                m_FriendList.Add(data.m_AccountID , data);
+            }
+
+            m_ReceivePendingList.Clear();
+            foreach (var data in recvList)
+            {
+                m_ReceivePendingList.Add(data.m_AccountID , data);
+            }
+
+            m_SendPendingList.Clear();
+            foreach (var data in sendList)
+            {
+                m_SendPendingList.Add(data.m_AccountID , data);
+            }
+        }
+
+        public CFriendAgent(CUser owner)
+        {
+            m_Owner = owner;
+        }
+
+        public _FriendData FindFriend(Int64 accountID)
+        {
+            if (m_FriendList.TryGetValue(accountID, out _FriendData retval))
+                return retval;
+
+            return null;
+        }
+
+        public List<_FriendData> GetFriendList()
+        {
+            return new List<_FriendData>(m_FriendList.Values);
+        }
+
+
+        public _FriendData FindSendUser(Int64 accountID)
+        {
+            if (m_SendPendingList.TryGetValue(accountID, out _FriendData retval))
+                return retval;
+
+            return null;
+        }
+
+        public List<_FriendData> GetSendPendingList()
+        {
+            return new List<_FriendData>(m_SendPendingList.Values);
+        }
+
+        public _FriendData FindReceiveUser(Int64 accountID)
+        {
+            if (m_ReceivePendingList.TryGetValue(accountID, out _FriendData retval))
+                return retval;
+
+            return null;
+        }
+
+        public List<_FriendData> GetRecvPendingList()
+        {
+            return new List<_FriendData>(m_ReceivePendingList.Values);
+        }
+
+
+        public void ReportFriendAccept(_FriendData friendData , bool isAccept)
+        {
+            if (FindFriend(friendData.m_AccountID) != null)
+                return;
+
+            if (isAccept)
+                m_FriendList.Add(friendData.m_AccountID , friendData);
+
+            if (m_ReceivePendingList.ContainsKey(friendData.m_AccountID))
+                m_ReceivePendingList.Remove(friendData.m_AccountID);
+
+            if (m_SendPendingList.ContainsKey(friendData.m_AccountID))
+                m_SendPendingList.Remove(friendData.m_AccountID);
+
+            //report
+            CNetManager.Instance.P2C_ReportFriendAccept(m_Owner.SessionKey , friendData , isAccept);
+        }
+
+        public void ReqBeRegisteredFriend(_FriendData friendData , bool isRegister)
+        {
+            if (isRegister)
+            {
+                if (FindReceiveUser(friendData.m_AccountID) != null)
+                    return;
+
+                m_ReceivePendingList.Add(friendData.m_AccountID , friendData);
+            }
+            else
+            {
+                if (FindReceiveUser(friendData.m_AccountID) == null)
+                    return;
+
+                m_ReceivePendingList.Remove(friendData.m_AccountID);
+            }
+
+            //report
+            CNetManager.Instance.P2C_ReportBeRegisteredFriend(m_Owner.SessionKey , friendData , isRegister);
+        }
+
+
+        public void ReportFriendDelete(_FriendData friendData)
+        {
+            if (m_FriendList.Remove(friendData.m_AccountID))
+                CNetManager.Instance.P2C_ReportFriendDelete(m_Owner.SessionKey, friendData);
+        }
+
+        public Packet_Result.Result ReqFriendDelete(Int64 targetID)
+        {
+            if (!m_FriendList.Remove(targetID))
+                return Packet_Result.Result.Friend_FriendDeleted;
+
+            CDBManager.Instance.QueryFriendDelete(m_Owner.DBGUID , m_Owner.SessionKey, m_Owner.AccountID, targetID);
+            return Packet_Result.Result.Success;
+        }
+
+        public void AfterQueryFriendDelete(_FriendData friendData, Packet_Result.Result result)
+        {
+            CNetManager.Instance.P2C_ResultFriendDelete(m_Owner.SessionKey , result , friendData);
+        }
+
+        public Packet_Result.Result ReqSendFriendRegister(long sessionKey , Int64 targetID , bool isRegister)
+        {
+            //valid
+            var friend_data = FindFriend(targetID);
+            if (friend_data != null)
+                return Packet_Result.Result.Friend_FriendRequest_Error;
+
+            if (targetID == m_Owner.AccountID)
+                return Packet_Result.Result.Friend_ToastMessage_14;
+
+            if (isRegister)
+            {
+                if (m_FriendList.Count >= CDefineTable.Instance.Find(eTableDefineType.Friend_Max_Friend_Count).m_Int64Value)
+                    return Packet_Result.Result.Friend_ToastMessage_02;
+
+                if (m_SendPendingList.Count >= CDefineTable.Instance.Find(eTableDefineType.Friend_Max_Friend_Request).m_Int64Value)
+                    return Packet_Result.Result.Friend_ToastMessage_04;
+
+                var recv_pending_data = FindReceiveUser(targetID);
+                if (recv_pending_data != null)
+                    return Packet_Result.Result.Friend_FriendRequest_Error;
+
+                var send_pending_data = FindSendUser(targetID);
+                if (send_pending_data != null)
+                    return Packet_Result.Result.Friend_FriendRequest_Error;
+            }
+            else
+            {
+                var send_pending_data = FindSendUser(targetID);
+                if (send_pending_data == null)
+                    return Packet_Result.Result.Friend_ToastMessage_14;
+            }
+
+            //process
+            CDBManager.Instance.QueryFriendRegister(m_Owner.DBGUID , sessionKey , m_Owner.AccountID , targetID, isRegister);
+            return Packet_Result.Result.Success;
+        }
+
+        public void AfterQueryFriendRegister(_FriendData friendData, bool isRegister, Packet_Result.Result result)
+        {
+            if (isRegister)
+            {
+                if (!m_SendPendingList.ContainsKey(friendData.m_AccountID))
+                    m_SendPendingList.Add(friendData.m_AccountID, friendData);
+            }
+            else
+            {
+                if (m_SendPendingList.ContainsKey(friendData.m_AccountID))
+                    m_SendPendingList.Remove(friendData.m_AccountID);
+            }
+
+            CNetManager.Instance.P2C_ResultFriendRegister(m_Owner.SessionKey, result, friendData, isRegister);
+        }
+
+        public Packet_Result.Result ReqFriendSearch(long sessionKey , string userName)
+        {
+            if (string.IsNullOrEmpty(userName))
+                return Packet_Result.Result.PacketError;
+
+            if (string.Equals(userName, m_Owner.PlayerData.m_Name))
+                return Packet_Result.Result.Friend_ToastMessage_14;
+
+            if (!CDefine.CheckValidNickname(userName))
+                return Packet_Result.Result.Friend_NotFoundFriend;
+
+            CDBManager.Instance.QueryFrinedSearch(m_Owner.DBGUID, sessionKey , userName);
+
+            return Packet_Result.Result.Success;
+        }
+
+        public Packet_Result.Result ReqFriendAccept(long sessionKey, long targetID, bool isAccept)
+        {
+            var receive_data = FindReceiveUser(targetID);
+            if (receive_data == null)
+                return Packet_Result.Result.Friend_ToastMessage_14;
+
+            if (isAccept)
+            {
+                if (m_FriendList.Count >= CDefineTable.Instance.Find(eTableDefineType.Friend_Max_Friend_Count).m_Int64Value)
+                    return Packet_Result.Result.Friend_ToastMessage_07;
+            }
+
+            //query
+            CDBManager.Instance.QueryFriendAccept(m_Owner.DBGUID , sessionKey , m_Owner.AccountID , targetID, isAccept);
+            
+            return Packet_Result.Result.Success;
+        }
+
+        public void AfterQueryFriendAccept(_FriendData friendData, bool isAccept, Packet_Result.Result result)
+        {
+            if (isAccept)
+            {
+                if (false == m_FriendList.ContainsKey(friendData.m_AccountID))
+                    m_FriendList.Add(friendData.m_AccountID, friendData);
+            }
+
+            m_SendPendingList.Remove(friendData.m_AccountID);
+            m_ReceivePendingList.Remove(friendData.m_AccountID);
+            
+            CNetManager.Instance.P2C_ResultFriendAccept(m_Owner.SessionKey , result , friendData, isAccept);
+        }
+
+        public void ReqRecvPendingFriendList(List<_FriendData> recvPendingList)
+        {
+            RefreshRecvPendingList(recvPendingList);
+            CNetManager.Instance.P2C_ResultRecvPendingFriendList(m_Owner.SessionKey , Packet_Result.Result.Success , GetRecvPendingList());
+        }
+
+        public void ReqSendPendingFriendList(List<_FriendData> sendPendingList)
+        {
+            RefreshSendPendingList(sendPendingList);
+            CNetManager.Instance.P2C_ResultSendPendingFriendList(m_Owner.SessionKey , Packet_Result.Result.Success , GetSendPendingList());
+        }
+
+        public void RefreshRecvPendingList(List<_FriendData> recvPendingList)
+        {
+            m_ReceivePendingList.Clear();
+            foreach(var iter in recvPendingList)
+                m_ReceivePendingList.Add(iter.m_AccountID , iter);
+        }
+
+        public void RefreshSendPendingList(List<_FriendData> sendPendingList)
+        {
+            m_SendPendingList.Clear();
+            foreach(var iter in sendPendingList)
+                m_SendPendingList.Add(iter.m_AccountID , iter);
+        }
+
+        public void ReqFriendList(List<_FriendData> friendList)
+        {
+            RefreshFriendList(friendList);
+            CNetManager.Instance.P2C_ResultFriendList(m_Owner.SessionKey , Packet_Result.Result.Success , GetFriendList());
+        }
+
+        public void RefreshFriendList(List<_FriendData> friendList)
+        {
+            m_FriendList.Clear();
+            foreach(var iter in friendList)
+            {
+                if (iter.m_UserState == eUserState.Max)
+                    iter.m_ForClientLogoutTime = SDateManager.Instance.DatediffSec(iter.m_LogoutTime , DateTime.UtcNow);
+
+                m_FriendList.Add(iter.m_AccountID , iter);
+            }
+        }
+    }
+}
---
+++ RuneAgent.cs
@@ -0,0 +1,270 @@
+﻿using System;
+using System.Collections.Generic;
+using SCommon;
+using Global;
+
+namespace PlayServer
+{
+    public class CRune
+    {
+        public int m_TotalCost;
+        public Dictionary<int, _RuneGradeData> m_Gradelist = new Dictionary<int, _RuneGradeData>();
+        public Dictionary<int, _RuneCostData> m_Costlist = new Dictionary<int, _RuneCostData>();
+
+
+        public _RuneData ConvertRuneData()
+        {
+            _RuneData retVal = new _RuneData();
+            retVal.m_TotalCost = m_TotalCost;
+            retVal.m_CostData = new List<_RuneCostData>(m_Costlist.Values);
+            retVal.m_GradeData = new List<_RuneGradeData>(m_Gradelist.Values);
+
+            return retVal;
+        }
+
+        public _RuneGradeData FindRuneGrade(int group_id)
+        {
+            if (m_Gradelist.TryGetValue(group_id, out _RuneGradeData retval))
+                return retval;
+
+            return null;
+        }
+
+        public _RuneCostData FindRuneCost(int table_id)
+        {
+            if (m_Costlist.TryGetValue(table_id, out _RuneCostData retval))
+                return retval;
+
+            return null;
+        }
+    }
+
+    public class CRuneAgent
+    {
+        private CUser m_Owner;
+
+        private CRune m_Rune = new CRune();
+
+        public CRuneAgent(CUser user)
+        {
+            m_Owner = user;
+        }
+
+        public _RuneData GetRuneData()
+        {
+            return m_Rune.ConvertRuneData();
+        }
+
+
+        public void Init(List<_RuneGradeData> grade_list, List<_RuneCostData> cost_list)
+        {
+            var default_cost = CRuneCostTable.Instance.DefaultData();
+            foreach (var iter in default_cost)
+                m_Rune.m_Costlist.Add(iter.m_TableID, iter);
+
+            var default_grade = CGradeTotalLevelTable.Instance.DefaultData();
+            foreach (var iter in default_grade)
+                m_Rune.m_Gradelist.Add(iter.m_GroupID, iter);
+
+            foreach (var iter in grade_list)
+                m_Rune.m_Gradelist[iter.m_GroupID] = iter;
+
+            foreach (var iter in cost_list)
+                m_Rune.m_Costlist[iter.m_TableID] = iter;
+
+            RefreshRuneCost();
+        }
+
+
+        public int GetRuneCost()
+        {
+            return m_Rune.m_TotalCost;
+        }
+
+        public void RefreshRuneCost()
+        {
+            int basecost = (int)CDefineTable.Instance.Find(eTableDefineType.Rune_BaseMaxCost).m_Int64Value;
+            foreach (var iter in m_Rune.m_Costlist)
+            {
+                var itval = iter.Value;
+                if (itval.m_IsRewarded)
+                    continue;
+
+                CRuneCostRecord record = CRuneCostTable.Instance.Find(itval.m_TableID);
+                if (record == null)
+                    continue;
+
+                basecost += record.m_GrowthCost;
+            }
+
+            m_Rune.m_TotalCost = basecost;
+        }
+
+        public _RuneCostData FindRuneCost(int table_id)
+        {
+            return m_Rune.FindRuneCost(table_id);
+        }
+
+        public _RuneGradeData FindRuneGrade(int group_id)
+        {
+            return m_Rune.FindRuneGrade(group_id);
+        }
+
+        public void GetRuneGradeAbils(ref Dictionary<eAbilBattleType, _AbilData> rAbils)
+        {
+            foreach (var iter in m_Rune.m_Gradelist)
+            {
+                var itval = iter.Value;
+                CGradeTotalLevelTable.Instance.GetGradeTotalLevelAbils(itval.m_GroupID, itval.m_Level, ref rAbils);
+            }
+        }
+
+        public Packet_Result.Result ReqItemRuneGradeUnlock(int group_id)
+        {
+            //valid
+            var runeGrade = m_Rune.FindRuneGrade(group_id);
+            if (runeGrade == null)
+                return Packet_Result.Result.PacketError;
+
+            CGradeTotalLevelRecord totalRecord = new CGradeTotalLevelRecord();
+            CGradeTotalLevelRecord nextTotalRecord = new CGradeTotalLevelRecord();
+            if (runeGrade.m_Level < 1)
+            {
+                totalRecord = CGradeTotalLevelTable.Instance.Find(group_id, 1);
+                nextTotalRecord = totalRecord;
+            }
+            else
+            {
+                totalRecord = CGradeTotalLevelTable.Instance.Find(group_id, runeGrade.m_Level);
+                nextTotalRecord = CGradeTotalLevelTable.Instance.Find(group_id, runeGrade.m_Level + 1);
+            }
+
+            if (totalRecord == null)
+                return Packet_Result.Result.PacketError;
+
+            if (nextTotalRecord == null)
+                return Packet_Result.Result.PacketError;
+
+            CGradeTotalLevelRecord endTotalRecord = CGradeTotalLevelTable.Instance.EndGradeTotalLevelRecord(group_id);
+            if (endTotalRecord == null)
+                return Packet_Result.Result.PacketError;
+
+            if (endTotalRecord.m_Level <= runeGrade.m_Level)
+                return Packet_Result.Result.PacketError;
+
+            CConditionRecord condRecord = CConditionTable.Instance.Find(nextTotalRecord.m_ConditionID);
+            if (condRecord == null)
+                return Packet_Result.Result.PacketError;
+
+            //condition
+            if (false == condRecord.ConditionCheck(m_Owner))
+                return Packet_Result.Result.PacketError;
+
+            //process
+            runeGrade.m_Level = nextTotalRecord.m_Level;
+
+            m_Owner.StatusAgent.Refresh(new List<eStatusCategory> { eStatusCategory.RuneGrade });
+
+            //db
+            CDBManager.Instance.QueryRuneUpdate(m_Owner.DBGUID, m_Owner.SessionKey, m_Owner.AccountID, new CRewardDBMerge(), new CRewardInfo(), runeGrade);
+
+            //log
+            var log = LogHelper.PrepareLog(eLog.rune_grade, m_Owner, null, 1);
+            log.SetTargetObj(runeGrade.m_GroupID.ToString(), -1, "");
+            log.SetSubStr(SJson.ObjectToJson(runeGrade));
+            CNetManager.Instance.P2L_ReportLogging(log);
+
+            return Packet_Result.Result.Success;
+        }
+
+        public Packet_Result.Result ReqItemRuneCostUnlock(int table_id)
+        {
+            //valid
+            var costData = m_Rune.FindRuneCost(table_id);
+            if (costData == null)
+                return Packet_Result.Result.PacketError;
+
+            if (costData.m_IsRewarded == true)
+                return Packet_Result.Result.PacketError;
+
+            var costRecord = CRuneCostTable.Instance.Find(table_id);
+            if (costRecord == null)
+                return Packet_Result.Result.DataError;
+
+            var condRecord = CConditionTable.Instance.Find(costRecord.m_ConditionID);
+            if (condRecord == null)
+                return Packet_Result.Result.DataError;
+
+            //condition
+            if (false == condRecord.ConditionCheck(m_Owner))
+                return Packet_Result.Result.PacketError;
+
+            //process
+            costData.m_IsRewarded = true;
+
+            RefreshRuneCost();
+
+            //db
+            CDBManager.Instance.QueryRuneCostUpdate(m_Owner.DBGUID, m_Owner.SessionKey, m_Owner.AccountID, new CRewardDBMerge(), new CRewardInfo(), costData);
+
+            //log
+            var log = LogHelper.PrepareLog(eLog.rune_cost, m_Owner, null, 1);
+            log.SetTargetObj(costRecord.m_TableID.ToString(), -1, "");
+            CNetManager.Instance.P2L_ReportLogging(log);
+
+            return Packet_Result.Result.Success;
+        }
+
+        public void AfterQueryRuneGrade(CRewardDBMerge dbtran, _RuneGradeData grade_data, Packet_Result.Result result)
+        {
+            dbtran.AfterQuery(m_Owner);
+            CNetManager.Instance.P2C_ResultItemRuneGradeUnlock(m_Owner.SessionKey, grade_data, result);
+        }
+
+        public void AfterQueryRuneCost(CRewardDBMerge dbtran, _RuneCostData cost_Data, Packet_Result.Result result)
+        {
+            dbtran.AfterQuery(m_Owner);
+            CNetManager.Instance.P2C_ResultItemRuneCostUnlock(m_Owner.SessionKey, cost_Data, m_Rune.m_TotalCost, result);
+        }
+
+        //cheat
+        public void cheat_RuneCostClear()
+        {
+            foreach (var iter in m_Rune.m_Costlist)
+            {
+                var itval = iter.Value;
+                if (itval.m_IsRewarded == false)
+                    continue;
+
+                itval.m_IsRewarded = false;
+                //db
+                CDBManager.Instance.QueryRuneCostUpdate(m_Owner.DBGUID, m_Owner.SessionKey, m_Owner.AccountID, new CRewardDBMerge(), new CRewardInfo(), itval);
+            }
+
+            RefreshRuneCost();
+
+            CNetManager.Instance.P2C_ReportRuneData(m_Owner.SessionKey, GetRuneData());
+            CNetManager.Instance.P2C_ResultCheat(m_Owner.SessionKey, Packet_Result.Result.Success);
+        }
+
+        //cheat
+        public void cheat_RuneGradeClear()
+        {
+            foreach (var iter in m_Rune.m_Gradelist)
+            {
+                var itval = iter.Value;
+                if (itval.m_Level == 0)
+                    continue;
+
+                itval.m_Level = 0;
+                m_Owner.StatusAgent.Refresh(new List<eStatusCategory> { eStatusCategory.RuneGrade });
+
+                //db
+                CDBManager.Instance.QueryRuneUpdate(m_Owner.DBGUID, m_Owner.SessionKey, m_Owner.AccountID, new CRewardDBMerge(), new CRewardInfo(), itval);
+            }
+
+            CNetManager.Instance.P2C_ReportRuneData(m_Owner.SessionKey, GetRuneData());
+            CNetManager.Instance.P2C_ResultCheat(m_Owner.SessionKey, Packet_Result.Result.Success);
+        }
+    }
+}
